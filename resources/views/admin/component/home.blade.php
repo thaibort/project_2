cc14d8fca68b8561ac2a28f480356719ba532a23
levucuong <76680667+levucuong@users.noreply.github.com>
2021-09-03 20:46:44 +0700

    layout

--- resources/views/admin/component/home.blade.php
+++ resources/views/admin/component/home.blade.php
@@ -1,48 +1,119 @@
 @extends('.admin.layout.master')
 @section('title','Trang chủ')
 @section('body')
-<div class="bg-white w-3/4 h-full">
-    <div class=" grid grid-cols-3 gap-6 bg-green-200 h-full w-full ">
-                        <div class="bg-green-400 border-red-300 m-5 academic-cap rounded-md ">
-                        <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
-                            </svg>
-                        Quản lý nhân viên </a>
+
+<div class="content-wrapper" style="min-height: 800px;">         
+            <div class="content-header ">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Dashboard</h1>
                         </div>
-                        <div class="bg-red-800 border-red-300 m-5 rounded-md">
-                        <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path>
-                            </svg>
-                       Quản lý khóa </a>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item active">
+                                    <font style="vertical-align: inherit;"></font>
+                                </li>
+                            </ol>
                         </div>
-                        <div class="bg-purple-300 border-red-300 m-5 rounded-md ">
-                        <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z" clip-rule="evenodd"></path>
-                            </svg>
-                         Quản lý ngành </a>
+                    </div>
+                </div>
+            </div>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-3 col-3">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>5</h3>
+                                    <p>Quản lý nhân viên</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-person-add"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
                         </div>
-                        <div class=" bg-purple-300 border-red-300 m-5 rounded-md">
-                        <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
-                            </svg>
-                        Quản lý lớp </a>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                                    <p>Quản lý khóa</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-pie-graph"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
                         </div>
-                        <div class="bg-red-800 border-red-300 m-5 rounded-md ">
-                        <a href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"></path>
-                            </svg>
-                        Quản lý sinh viên </a>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Quản lý ngành</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-majors"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">More info </font>
+                                    </font><i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
                         </div>
-                        <div class="bg-green-400 border-red-300 m-5 rounded-md ">
-                        <a href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-                            </svg>
-                        Quản lý hóa đơn </a>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-danger">
+                                <div class="inner">
+                                    <h3>65</h3>
+                                    <p>Quản lý lớp</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion "></i>
+                                </div>
+                                <a href="#" class="small-box-footer">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">More info </font>
+                                    </font><i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                        </div>        
+                    </div>
+                    <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Quản lý sinh viên</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-person-add"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">More info </font>
+                                    </font><i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
                         </div>
+                        <div class="col-lg-3 col-6">
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+                                    <p>Quản lý hóa đơn</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion "></i>
+                                </div>
+                                <a href="#" class="small-box-footer">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">More info </font>
+                                    </font><i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                </div>
+            </section>           
+        </div>
+
 @endsection
