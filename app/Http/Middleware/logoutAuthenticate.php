d6f80214a5960de0078f4962ac31f31cea1e4fe5
thaibort <76679347+thaibort@users.noreply.github.com>
2021-09-06 13:10:23 +0700

    skad

---
+++ app/Http/Middleware/logoutAuthenticate.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class logoutAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::guard('admin')->check()){
+            return redirect()->route('home');
+        }
+        else{
+            return redirect()->route('login');
+        }
+    }
+}
