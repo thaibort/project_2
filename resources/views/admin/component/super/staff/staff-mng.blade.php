b411580326bddd3549c3c3f857a9ad36b6351104
thaibort <76679347+thaibort@users.noreply.github.com>
2021-09-08 21:07:58 +0700

    fix-invoices

--- resources/views/admin/component/super/staff/staff-mng.blade.php
+++ resources/views/admin/component/super/staff/staff-mng.blade.php
@@ -34,7 +34,29 @@
                     <form class="w-full h-full bg-red-200" action='{{url("admin/staff/{$res->id}")}}' method="post">
                         @csrf
                         @method("DELETE")
-                        <button type="submit" class="edit_hover">Xóa</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            Xóa
+                        </button>
+
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Xác nhận</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Bạn có chắc muốn xóa
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                                        <button type="submit" class="btn btn-primary">Xác nhận</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </form>
                 </td>
             </tr>
