63b2fa35fec8bc92a4a1c15d4d3f4e77cd74c056
thaibort <76679347+thaibort@users.noreply.github.com>
2021-09-21 16:54:16 +0700

    paginate

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,38 +1,43 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+{{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
     <title>Bkacad</title>
 </head>
-<body>
-    <div>
-        <form action='{{url("/")}}'>
+<body class="relative min-h-screen p-5">
+<div></div>
+    <div class="border border-black w-1/5 rounded-full">
+        <form action='{{url("/")}}' class="w-full h-8 flex items-center justify-between">
             @csrf
-            <input type="text" name="kw" value="{{$kw}}">
-            <button type="submit">search</button>
+            <input type="text" name="kw" value="{{$kw}}" class="outline-none w-5/6 h-8 rounded-l-full pl-3">
+            <button type="submit" class="bg-green-200 hover:bg-blue-200 h-8 w-8 flex justify-center items-center rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </button>
         </form>
     </div>
-    <table>
+    <table class="table-auto w-full bg-gray-200 mt-10">
         <tr>
-            <th>Mã sinh viên</th>
-            <th>Ngành</th>
-            <th>Khóa</th>
-            <th>Lớp</th>
-            <th>Họ và tên</th>
-            <th>Tình trạng học phí</th>
-            <th>Hành động</th>
+            <th class="h-14 bg-blue-700 border text-white">Mã sinh viên</th>
+            <th class="h-14 bg-blue-700 border text-white">Ngành</th>
+            <th class="h-14 bg-blue-700 border text-white">Khóa</th>
+            <th class="h-14 bg-blue-700 border text-white">Lớp</th>
+            <th class="h-14 bg-blue-700 border text-white">Họ và tên</th>
+            <th class="h-14 bg-blue-700 border text-white">Tình trạng học phí</th>
+            <th class="h-14 bg-blue-700 border text-white">Hành động</th>
         </tr>
         @forelse($rs as $res)
             <tr>
-                <td>{{$res -> id}}</td>
-                <td>{{$res -> vocation}}</td>
-                <td>{{$res -> schoolYear}}</td>
-                <td>{{$res -> className}}</td>
-                <td>{{$res -> name}}</td>
-                <td>
+                <td class="h-8 border ">{{$res -> id}}</td>
+                <td class="h-8 border ">{{$res -> vocation}}</td>
+                <td class="h-8 border ">{{$res -> schoolYear}}</td>
+                <td class="h-8 border ">{{$res -> className}}</td>
+                <td class="h-8 border ">{{$res -> name}}</td>
+                <td class="h-8 border ">
                     {{$res -> stagesPresent <= $res -> totalStages
                         ? 'Đã nộp'
                         : 'Nợ '.($res -> stagesPresent - $res -> totalStages).' tháng'
@@ -48,5 +53,8 @@
             </tr>
         @endforelse
     </table>
+    <div class="w-full pl-20 absolute bottom-10 right-10">
+        {{$rs->links()}}
+    </div>
 </body>
 </html>
