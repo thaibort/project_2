9f821ecf692c15e29884851451f185fa88faa397
thaibort <76679347+thaibort@users.noreply.github.com>
2021-09-21 08:23:32 +0700

    update-modal-delete

--- resources/views/admin/component/super/scholarship/scholarship-mng.blade.php
+++ resources/views/admin/component/super/scholarship/scholarship-mng.blade.php
@@ -17,45 +17,47 @@
         </thead>
         <tbody>
         @forelse($rs as $res)
-            <tr>
-                <td>{{$res -> type}}</td>
-                <td>{{$res -> money}}</td>
-                <td class="bg-blue">
-                    <form class="w-full h-full bg-blue-200 " action='{{url("admin/upscholarship/{$res->id}")}}'>
-                        @csrf
-                        <button type="submit" class="edit_hover border-0 bg-blue text-white btn btn-outline-secondary mrt-5">Sửa</button>
-                    </form>
-                </td>
-                <td class="bg-red">
-                    <form class="w-full h-full bg-red-200 " action='{{url("admin/scholarship/{$res->id}")}}' method="post">
-                        @csrf
-                        @method("DELETE")
-                        <button type="button" class="btn btn-primary border-0 bg-red text-white btn btn-outline-secondary mrt-5" data-toggle="modal" data-target="#exampleModal">
-                            Xóa
-                        </button>
+            @if($res -> id !== 0)
+                <tr>
+                    <td>{{$res -> type}}</td>
+                    <td>{{$res -> money}}</td>
+                    <td class="bg-blue">
+                        <form class="w-full h-full bg-blue-200 " action='{{url("admin/upscholarship/{$res->id}")}}'>
+                            @csrf
+                            <button type="submit" class="edit_hover border-0 bg-blue text-white btn btn-outline-secondary mrt-5">Sửa</button>
+                        </form>
+                    </td>
+                    <td class="bg-red">
+                        <form class="w-full h-full bg-red-200 " action='{{url("admin/scholarship/{$res->id}")}}' method="post">
+                            @csrf
+                            @method("DELETE")
+                            <button type="button" class="btn btn-primary border-0 bg-red text-white btn btn-outline-secondary mrt-5" data-toggle="modal" data-target="#exampleModal">
+                                Xóa
+                            </button>
 
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Xác nhận</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        Bạn có chắc muốn xóa
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
-                                        <button type="submit" class="btn btn-primary">Xác nhận</button>
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Xác nhận</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Bạn có chắc muốn xóa
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                                            <button type="submit" class="btn btn-primary">Xác nhận</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
-                </td>
-            </tr>
+                        </form>
+                    </td>
+                </tr>
+            @endif
         @empty
             <tr><td colspan="4"> Không có dữ liệu </td></tr>
         @endforelse
--- resources/views/admin/component/super/school_year/school-year-mng.blade.php
+++ resources/views/admin/component/super/school_year/school-year-mng.blade.php
@@ -35,9 +35,29 @@
                     <form class="w-full h-full -p-5" action='{{url("admin/schyear/{$res->id}")}}' method="post">
                         @csrf
                         @method("DELETE")
-                        <button type="submit" class="btn btn-primary border-0 bg-red text-white btn btn-outline-secondary mrt-5" data-toggle="modal" data-target="#exampleModal">
+                        <button type="button" class="btn btn-primary border-0 bg-red text-white btn btn-outline-secondary mrt-5" data-toggle="modal" data-target="#a{{$res->id}}">
                             Xóa
                         </button>
+
+                        <div class="modal fade text-black-50" id="a{{$res -> id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Xác nhận</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Bạn có chắc muốn xóa
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                                        <button type="submit" class="btn btn-primary">Xác nhận</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </form>
                 </td>
             </tr>
