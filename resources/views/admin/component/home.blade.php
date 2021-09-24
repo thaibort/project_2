2c5d42cf2cee14611e7556d1ef90ac2fd922abb7
cvu31594 <90102165+cvu31594@users.noreply.github.com>
2021-09-24 01:57:31 -0700

    a

--- resources/views/admin/component/home.blade.php
+++ resources/views/admin/component/home.blade.php
@@ -1,110 +1,148 @@
 @extends('.admin.layout.master')
 @section('title','Trang chủ')
 @section('content')
-<div >
-            <div class="content-header ">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Trang chủ</h1>
-                        </div>
-                        <div class="col-md-3">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item active col-md-3">
-                                    <font style="vertical-align: inherit;"></font>
-                                </li>
-                            </ol>
-                        </div>
-                    </div>
+<div>
+    <div class="content-header ">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Trang chủ</h1>
                 </div>
-             </div>
-                <div class="container-fluid">
-                    <div class="row">
-                        @if(session()->get('admin.level') == 0)
-                            <div class="col-md-3">
-                                <div class="small-box bg-success">
-                                    <div class="inner">
-                                        <h3>{{$year}}<sup style="font-size: 20px"></sup></h3>
-                                        <p>Khóa</p>
-                                    </div>
-                                    <div class="icon">
-                                        <i class="ion fas fa-chart-pie"></i>
-                                    </div>
-                                    <a href="{{url('admin/schyear')}}" class="small-box-footer"> Xem thêm <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                            <br>
-                            <div class="col-md-3">
-                                <div class="small-box bg-danger">
-                                    <div class="inner">
-                                        <h3>{{$class}}</h3>
-                                        <p>Lớp</p>
-                                    </div>
-                                    <div class="icon">
-                                        <i class="ion fas fa-school"></i>
-                                    </div>
-                                    <a href="{{url('admin/class')}}" class="small-box-footer">
-                                        <font style="vertical-align: inherit;">
-                                            <font style="vertical-align: inherit;">Xem thêm </font>
-                                        </font><i class="fas fa-arrow-circle-right"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        @endif
-                        <div class="col-md-3">
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>{{$student}}</h3>
-                                    <p>Sinh viên</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion fas fa-user-graduate"></i>
-                                </div>
-                                <a href="{{url('admin/student')}}" class="small-box-footer">
-                                    <font style="vertical-align: inherit;">
-                                        <font style="vertical-align: inherit;">Xem thêm </font>
-                                    </font><i class="fas fa-arrow-circle-right"></i>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="small-box bg-primary">
-                                <div class="inner">
-                                    <h3>{{$paid}}</h3>
-                                    <p>Sinh viên đã nộp học phí</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion fas fa-file-invoice"></i>
-                                </div>
-                                <a href="{{url('admin/invoice')}}" class="small-box-footer">
-                                    <font style="vertical-align: inherit;">
-                                        <font style="vertical-align: inherit;">Xem thêm </font>
-                                    </font><i class="fas fa-arrow-circle-right"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-md-3">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active col-md-3">
+                            <font style="vertical-align: inherit;"></font>
+                        </li>
+                    </ol>
                 </div>
-            </section>
+            </div>
         </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            @if(session()->get('admin.level') == 0)
+            <div class="col-md-3">
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{$year}}<sup style="font-size: 20px"></sup></h3>
+                        <p>Khóa</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion fas fa-chart-pie"></i>
+                    </div>
+                    <a href="{{url('admin/schyear')}}" class="small-box-footer"> Xem thêm <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <br>
+            <div class="col-md-3">
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{$class}}</h3>
+                        <p>Lớp</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion fas fa-school"></i>
+                    </div>
+                    <a href="{{url('admin/class')}}" class="small-box-footer">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Xem thêm </font>
+                        </font><i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            @endif
+            <div class="col-md-3">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{$student}}</h3>
+                        <p>Sinh viên</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion fas fa-user-graduate"></i>
+                    </div>
+                    <a href="{{url('admin/student')}}" class="small-box-footer">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Xem thêm </font>
+                        </font><i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="small-box bg-primary">
+                    <div class="inner">
+                        <h3>{{$paid}}</h3>
+                        <p>Sinh viên đã nộp học phí</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion fas fa-file-invoice"></i>
+                    </div>
+                    <a href="{{url('admin/invoice')}}" class="small-box-footer">
+                        <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Xem thêm </font>
+                        </font><i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+</div>
+<div class="w-full card bg-gradient-success">
+              <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
+
+                <h3 class="card-title">
+                  <i class="far fa-calendar-alt"></i>
+                  Calendar
+                </h3>
+                <!-- tools card -->
+                <div class="card-tools">
+                  <!-- button with a dropdown -->
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" data-offset="-52">
+                      <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="dropdown-menu" role="menu">
+                      <a href="#" class="dropdown-item">Add new event</a>
+                      <a href="#" class="dropdown-item">Clear events</a>
+                      <div class="dropdown-divider"></div>
+                      <a href="#" class="dropdown-item">View calendar</a>
+                    </div>
+                  </div>
+                  <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+                <!-- /. tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body pt-0">
+                <!--The calendar -->
+                <div id="calendar" style="width: 100%"><div class="bootstrap-datetimepicker-widget usetwentyfour"><ul class="list-unstyled"><li class="show"><div class="datepicker"><div class="datepicker-days" style=""><table class="table table-sm"><thead><tr><th class="prev" data-action="previous"><span class="fa fa-chevron-left" title="Previous Month"></span></th><th class="picker-switch" data-action="pickerSwitch" colspan="5" title="Select Month">September 2021</th><th class="next" data-action="next"><span class="fa fa-chevron-right" title="Next Month"></span></th></tr><tr><th class="dow">Su</th><th class="dow">Mo</th><th class="dow">Tu</th><th class="dow">We</th><th class="dow">Th</th><th class="dow">Fr</th><th class="dow">Sa</th></tr></thead><tbody><tr><td data-action="selectDay" data-day="08/29/2021" class="day old weekend">29</td><td data-action="selectDay" data-day="08/30/2021" class="day old">30</td><td data-action="selectDay" data-day="08/31/2021" class="day old">31</td><td data-action="selectDay" data-day="09/01/2021" class="day">1</td><td data-action="selectDay" data-day="09/02/2021" class="day">2</td><td data-action="selectDay" data-day="09/03/2021" class="day">3</td><td data-action="selectDay" data-day="09/04/2021" class="day weekend">4</td></tr><tr><td data-action="selectDay" data-day="09/05/2021" class="day weekend">5</td><td data-action="selectDay" data-day="09/06/2021" class="day">6</td><td data-action="selectDay" data-day="09/07/2021" class="day">7</td><td data-action="selectDay" data-day="09/08/2021" class="day">8</td><td data-action="selectDay" data-day="09/09/2021" class="day">9</td><td data-action="selectDay" data-day="09/10/2021" class="day">10</td><td data-action="selectDay" data-day="09/11/2021" class="day weekend">11</td></tr><tr><td data-action="selectDay" data-day="09/12/2021" class="day weekend">12</td><td data-action="selectDay" data-day="09/13/2021" class="day">13</td><td data-action="selectDay" data-day="09/14/2021" class="day">14</td><td data-action="selectDay" data-day="09/15/2021" class="day">15</td><td data-action="selectDay" data-day="09/16/2021" class="day">16</td><td data-action="selectDay" data-day="09/17/2021" class="day">17</td><td data-action="selectDay" data-day="09/18/2021" class="day weekend">18</td></tr><tr><td data-action="selectDay" data-day="09/19/2021" class="day weekend">19</td><td data-action="selectDay" data-day="09/20/2021" class="day">20</td><td data-action="selectDay" data-day="09/21/2021" class="day">21</td><td data-action="selectDay" data-day="09/22/2021" class="day">22</td><td data-action="selectDay" data-day="09/23/2021" class="day active today">23</td><td data-action="selectDay" data-day="09/24/2021" class="day">24</td><td data-action="selectDay" data-day="09/25/2021" class="day weekend">25</td></tr><tr><td data-action="selectDay" data-day="09/26/2021" class="day weekend">26</td><td data-action="selectDay" data-day="09/27/2021" class="day">27</td><td data-action="selectDay" data-day="09/28/2021" class="day">28</td><td data-action="selectDay" data-day="09/29/2021" class="day">29</td><td data-action="selectDay" data-day="09/30/2021" class="day">30</td><td data-action="selectDay" data-day="10/01/2021" class="day new">1</td><td data-action="selectDay" data-day="10/02/2021" class="day new weekend">2</td></tr><tr><td data-action="selectDay" data-day="10/03/2021" class="day new weekend">3</td><td data-action="selectDay" data-day="10/04/2021" class="day new">4</td><td data-action="selectDay" data-day="10/05/2021" class="day new">5</td><td data-action="selectDay" data-day="10/06/2021" class="day new">6</td><td data-action="selectDay" data-day="10/07/2021" class="day new">7</td><td data-action="selectDay" data-day="10/08/2021" class="day new">8</td><td data-action="selectDay" data-day="10/09/2021" class="day new weekend">9</td></tr></tbody></table></div><div class="datepicker-months" style="display: none;"><table class="table-condensed"><thead><tr><th class="prev" data-action="previous"><span class="fa fa-chevron-left" title="Previous Year"></span></th><th class="picker-switch" data-action="pickerSwitch" colspan="5" title="Select Year">2021</th><th class="next" data-action="next"><span class="fa fa-chevron-right" title="Next Year"></span></th></tr></thead><tbody><tr><td colspan="7"><span data-action="selectMonth" class="month">Jan</span><span data-action="selectMonth" class="month">Feb</span><span data-action="selectMonth" class="month">Mar</span><span data-action="selectMonth" class="month">Apr</span><span data-action="selectMonth" class="month">May</span><span data-action="selectMonth" class="month">Jun</span><span data-action="selectMonth" class="month">Jul</span><span data-action="selectMonth" class="month">Aug</span><span data-action="selectMonth" class="month active">Sep</span><span data-action="selectMonth" class="month">Oct</span><span data-action="selectMonth" class="month">Nov</span><span data-action="selectMonth" class="month">Dec</span></td></tr></tbody></table></div><div class="datepicker-years" style="display: none;"><table class="table-condensed"><thead><tr><th class="prev" data-action="previous"><span class="fa fa-chevron-left" title="Previous Decade"></span></th><th class="picker-switch" data-action="pickerSwitch" colspan="5" title="Select Decade">2020-2029</th><th class="next" data-action="next"><span class="fa fa-chevron-right" title="Next Decade"></span></th></tr></thead><tbody><tr><td colspan="7"><span data-action="selectYear" class="year old">2019</span><span data-action="selectYear" class="year">2020</span><span data-action="selectYear" class="year active">2021</span><span data-action="selectYear" class="year">2022</span><span data-action="selectYear" class="year">2023</span><span data-action="selectYear" class="year">2024</span><span data-action="selectYear" class="year">2025</span><span data-action="selectYear" class="year">2026</span><span data-action="selectYear" class="year">2027</span><span data-action="selectYear" class="year">2028</span><span data-action="selectYear" class="year">2029</span><span data-action="selectYear" class="year old">2030</span></td></tr></tbody></table></div><div class="datepicker-decades" style="display: none;"><table class="table-condensed"><thead><tr><th class="prev" data-action="previous"><span class="fa fa-chevron-left" title="Previous Century"></span></th><th class="picker-switch" data-action="pickerSwitch" colspan="5">2000-2090</th><th class="next" data-action="next"><span class="fa fa-chevron-right" title="Next Century"></span></th></tr></thead><tbody><tr><td colspan="7"><span data-action="selectDecade" class="decade old" data-selection="2006">1990</span><span data-action="selectDecade" class="decade" data-selection="2006">2000</span><span data-action="selectDecade" class="decade active" data-selection="2016">2010</span><span data-action="selectDecade" class="decade active" data-selection="2026">2020</span><span data-action="selectDecade" class="decade" data-selection="2036">2030</span><span data-action="selectDecade" class="decade" data-selection="2046">2040</span><span data-action="selectDecade" class="decade" data-selection="2056">2050</span><span data-action="selectDecade" class="decade" data-selection="2066">2060</span><span data-action="selectDecade" class="decade" data-selection="2076">2070</span><span data-action="selectDecade" class="decade" data-selection="2086">2080</span><span data-action="selectDecade" class="decade" data-selection="2096">2090</span><span data-action="selectDecade" class="decade old" data-selection="2106">2100</span></td></tr></tbody></table></div></div></li><li class="picker-switch accordion-toggle"></li></ul></div></div>
+              </div>
+              <!-- /.card-body -->
+            </div>
 @endsection
 
 @section('js')
 <scipt>
-        <script>
-            $(document).ready( function () {
-                $('#ccc').remove();
-            } );
-        </script>
-        @if(session()->get('admin.level') == 1)
-            $(document).ready( function () {
-            $('#mng').remove();
-            } );
-        @endif
-        @if(session()->get('admin.level') == 0)
-            $(document).ready( function () {
-            $('#stainfor').remove();
-            } );
-        @endif
+    <script>
+    $(document).ready(function() {
+        $('#ccc').remove();
+    });
+    </script>
+    @if(session()->get('admin.level') == 1)
+    $(document).ready( function () {
+    $('#mng').remove();
+    } );
+    @endif
+    @if(session()->get('admin.level') == 0)
+    $(document).ready( function () {
+    $('#stainfor').remove();
+    } );
+    @endif
 </scipt>
 @endsection
--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,36 +1,56 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
-{{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
-    <title>Bkacad</title>
+    {{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <title>BKACAD</title>
 </head>
-<body class="relative min-h-screen p-5">
-<div></div>
-    <div class="border border-black w-1/5 rounded-full d-flex justify-content-end">
-        <form action='{{url("/")}}' class="w-full h-8 flex items-center justify-between">
-            @csrf
-            <input type="text" name="kw" value="{{$kw}}" class="outline-none w-5/6 h-8 rounded-l-full pl-3">
-            <button type="submit" class="bg-green-200 hover:bg-blue-200 h-8 w-8 flex justify-center items-center rounded-full">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-            </button>
-        </form>
+<body>
+    <div class="w-full bg-blue-200 h-14 flex justify-between items-center px-5">
+        <div class="w-60 h-160">
+            <a href=" https://www.bkacad.com/" title="BKACAD" class="logo">
+                <img class="h-10 w-55" src="https://www.bkacad.com/images/config/logo_1591255072.png" alt="logo">
+            </a>
+        </div>
+        <div class="flex mr-5 justify-center space-x-4 ">
+            <div class="order-last mr-10 hover:text-blue-700 font-bold h-14 flex items-center w-20 justify-center">
+                <a class="block" href="#"> Trang chủ</a>
+            </div>
+            <div class="order-last mr-10 hover:text-blue-700 font-bold h-14 flex items-center w-20 justify-center">
+                <a class="block" href="#"> Phiếu thu</a>
+            </div>
+        </div>
+        <div class="border border-black w-1/5 rounded-full flex justify-end h-10 items-center">
+            <form action='{{url("/")}}' class=" h-8 flex items-center w-full ">
+                @csrf
+                <input type="text" name="kw" value="{{$kw}}" class="outline-none w-5/6 h-9 rounded-l-full pl-3">
+                <button type="submit"
+                    class="bg-green-200 hover:bg-blue-200 h-8 w-10 flex justify-center items-center rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+            </form>
+        </div>
     </div>
-    <table class="table-auto w-full bg-gray-100 mt-10" cellpadding="0" cellspacing="0">
-        <tr>
-            <th class="h-14 bg-blue-700 border-2 border-black text-white">Mã sinh viên</th>
-            <th class="h-14 bg-blue-700 border-2 border-black text-white">Ngành</th>
-            <th class="h-14 bg-blue-700 border-2 border-black text-white">Khóa</th>
-            <th class="h-14 bg-blue-700 border-2 border-black text-white">Lớp</th>
-            <th class="h-14 bg-blue-700 border-2 border-black text-white">Họ và tên</th>
-            <th class="h-14 bg-blue-700 border-2 border-black text-white">Tình trạng học phí</th>
-            <th class="h-14 bg-blue-700 border-2 border-black text-white">Hành động</th>
-        </tr>
-        @forelse($rs as $res)
+    <div class="container mx-auto">
+        <table class=" mr-10 table-auto w-full bg-gray-100 mt-10 text-center" cellpadding="0" cellspacing="0">
+            <tr>
+                <th class="h-14 bg-blue-700 border-2 border-black text-white">Mã sinh viên</th>
+                <th class="h-14 bg-blue-700 border-2 border-black text-white">Ngành</th>
+                <th class="h-14 bg-blue-700 border-2 border-black text-white">Khóa</th>
+                <th class="h-14 bg-blue-700 border-2 border-black text-white">Lớp</th>
+                <th class="h-14 bg-blue-700 border-2 border-black text-white">Họ và tên</th>
+                <th class="h-14 bg-blue-700 border-2 border-black text-white">Tình trạng học phí</th>
+                <th class="h-14 bg-blue-700 border-2 border-black text-white">Hành động</th>
+            </tr>
+            @forelse($rs as $res)
             <tr class="hover:bg-gray-300">
                 <td class="h-8 border-2 border-black text-center">{{$res -> id}}</td>
                 <td class="h-8 border-2 border-black text-center">{{$res -> vocation}}</td>
@@ -47,14 +67,19 @@
                     <a href='{{url("/toinvoice/{$res -> id}")}}'>Tổng phiếu thu</a>
                 </td>
             </tr>
-        @empty
+            @empty
             <tr class="h-8 border-2 border-black text-center">
                 <td colspan="8" class="">Không có dữ liệu sinh viên</td>
             </tr>
-        @endforelse
-    </table>
-    <div class="w-full pl-20 absolute bottom-10 right-10">
-        {{$rs->links()}}
+            @endforelse
+        </table>
+        <div class="w-full pl-20 absolute bottom-10 right-10">
+            {{$rs->links()}}
+        </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"
+        integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
