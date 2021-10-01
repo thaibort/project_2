7aa100fd09e4db37d99a33394f8909dc2a239e2a
cvu31594 <90102165+cvu31594@users.noreply.github.com>
2021-10-01 13:07:21 +0700

    hjvj

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,21 +1,45 @@
 @extends('client.layout.master')
-@section('title','Trang chủ')
+@section('title','Trang Chủ')
 @section('menu')
-    <div class="order-last  border-red-500 border-b-2 mr-10 hover:text-blue-700 font-bold h-14 flex items-center w-20 justify-center">
-        <form action="{{url('/')}}">
-            @csrf
-            <input type="text" hidden name="mode" value="0" class="bg-opacity-0">
-            <input type="submit" value="Trang chủ" class="bg-blue-200">
-        </form>
-    </div>
-    <div class="order-last mr-10 hover:text-blue-700 font-bold h-14 flex items-center w-20 justify-center">
-        <form action="{{url('invoice')}}">
-            @csrf
-            <input type="text" hidden name="mode" value="1" class="bg-opacity-0">
-            <input type="submit" value="Phiếu thu" class="bg-blue-200">
-        </form>
-    </div>
+<div
+    class="order-last  border-red-500 border-b-2 mr-10 hover:text-blue-700 font-bold h-14 flex items-center w-20 justify-center">
+    <form action="{{url('/')}}">
+        @csrf
+        <input type="text" hidden name="mode" value="0" class="bg-opacity-0">
+        <input type="submit" value="Trang Chủ" class="bg-blue-200 text-lg">
+    </form>
+</div>
+<div class="order-last mr-10 hover:text-blue-700 font-bold h-14 flex items-center w-20 justify-center">
+    <form action="{{url('invoice')}}">
+        @csrf
+        <input type="text" hidden name="mode" value="1" class="bg-opacity-0">
+        <input type="submit" value="Phiếu Thu" class="bg-blue-200 text-lg">
+    </form>
+</div>
 @endsection
 @section('body')
-    <h1>Home</h1>
-@endsection
+<div class="owl-item active" style="width: 1475px;">
+    <div class="item">
+        <a class="item-img cover hide" href=""
+            style="background:url(https://www.bkacad.com/images/slideshow/2020/08/06/slideshow_large/banner-slide-1_1596680312.jpg) no-repeat center center;"
+            title="Học viện uy tín nhất khu vực Châu Á - Thái Bình Dương"></a>
+        <a class="item-cover " href="" title="Học viện uy tín nhất khu vực Châu Á - Thái Bình Dương">
+            <img class="img-responsive"
+                src="https://www.bkacad.com/images/slideshow/2020/08/06/slideshow_large/banner-slide-1_1596680312.jpg"
+                alt="Học viện uy tín nhất khu vực Châu Á - Thái Bình Dương">
+        </a>
+    </div>
+    <div class="flex justify-between">
+        <div>
+        <h3 class="text-xl font-semibold text-red-600 "> HỌC VIỆN CÔNG NGHỆ BKACAD</h3>
+            <h1 class="text-lg font-medium text-blue-500"> Hệ chứng chỉ & Hệ chuyên gia  quốc tế</h1>
+            <h1 class="text-sm">Địa chỉ: P214, Tòa nhà A17 Bách Khoa, 17 Tạ Quang Bửu, Hai Bà Trưng, Hà Nội</h1>
+            <h1 class="text-sm">ĐT: 036 779 1116 - 0243 623 1023 </h1>
+        </div>
+        <div class="pt-6">
+            <h1 class="text-lg font-medium text-blue-500"> Cơ sở TP. Hồ Chí Minh  </h1>
+            <h1 class="text-sm"> Địa chỉ : Số 15, Đường Chợ Lớn, Phường 11, Quận 6, TP. Hồ Chí Minh</h1>
+            <h1 class="text-sm">   ĐT: 0283 755 9979 - 0918 55 46 55 </h1>
+        </div>
+    </div>
+    @endsection
--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -9,6 +9,7 @@
     {{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
     <title>@yield('title')</title>
 </head>
+
 <body>
     <div class="w-full bg-blue-200 h-14 flex justify-between items-center px-5">
         <div class="w-60 h-160">
@@ -20,33 +21,32 @@
             @yield('menu')
         </div>
         @if($mode == 1)
-            <div class=" w-1/5 rounded-full flex justify-end h-10 items-center">
-                <form action='{{url("invoice")}}' class=" h-8 flex items-center w-full z-10">
-                    @csrf
-                    <input type="text" name="mode" hidden value="1">
-                    <input type="text" name="kw" value="{{$kw}}" class="focus:ring z-20 outline-none w-5/6 h-10 rounded-l-full pl-3" placeholder="Tìm kiếm ...">
-                    <button
-                        type="submit"
-                        class="bg-green-300 hover:bg-green-400 h-10 w-1/6 flex justify-center items-center rounded-r-full"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                    </button>
-                </form>
-            </div>
+        <div class=" w-1/5 rounded-full flex justify-end h-10 items-center">
+            <form action='{{url("invoice")}}' class=" h-8 flex items-center w-full z-10">
+                @csrf
+                <input type="text" name="mode" hidden value="1">
+                <input type="text" name="kw" value="{{$kw}}"
+                    class="focus:ring z-20 outline-none w-5/6 h-10 rounded-l-full pl-3" placeholder="Tìm kiếm ...">
+                <button type="submit"
+                    class="bg-green-300 hover:bg-green-400 h-10 w-1/6 flex justify-center items-center rounded-r-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+            </form>
+        </div>
         @endif
         @if($mode == 0)
-            <div class="w-1/5"></div>
+        <div class="w-1/5"></div>
         @endif
     </div>
 
     @yield('body')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"
         integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous">
-</script>
+    </script>
 </body>
 
 </html>
